3b254ec74e0a15d878ca85f3f92adefc9f14fa00
anitavrn <anitavrnka@gmail.com>
2024-12-10 18:56:46 +0700

    Add files via upload

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+            'name' => 'Administrator',
+            'email' => 'admin@admin',
+            'password' => bcrypt('adminadmin'),
+            ],
+            [
+            'name' => 'Anita',
+            'email' => 'anitavrnka@gmail.com',
+            'password' => bcrypt('anita2012'),
+            ]
+
+
+        ]);
+    }
+}
